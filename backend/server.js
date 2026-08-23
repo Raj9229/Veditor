5409b4042b9a4b51590ea4c2f76868b62cbe8726
Raj Gupta <krishnagupta9229@gmail.com>
2026-08-23 23:59:55 +0530

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,12 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "Hello World", success:true })
 })
 
-app.get()
 
+
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ message: "ok", success:true })
+})
 
 
 
