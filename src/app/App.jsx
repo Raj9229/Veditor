107b22bec0ac1e9c2960538a58ed73d1fd9d8137
Raj Gupta <krishnagupta9229@gmail.com>
2026-08-23 23:06:13 +0530

    ui design -user and editor

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,7 +5,19 @@ function App() {
 
   return (
 
-    <main className="w-full h-screen bg-gray-900"></main>
+    <main className="w-full h-screen bg-gray-900 flex gap-4 p-4">
+
+      <aside className="h-full w-1/4 bg-amber-50 rounded-lg">
+      </aside>
+
+      <section className="w-3/4 bg-neutral-800 rounded-lg">
+      
+      </section>
+
+
+
+
+    </main>
 
     
 
