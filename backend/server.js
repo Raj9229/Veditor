e0d410358a7547c9a358a04844b5ba7e0255fcb6
Raj Gupta <krishnagupta9229@gmail.com>
2026-08-23 23:57:44 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from "express"
+import {createServer} from "http"
+import {Server} from "socket.io"
+import {YSocketIO} from "y-socket.io/dist/server"
+
+
+const app = express()
+const httpServer = createServer(app)
+
+
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello World", success:true })
+})
+
+app.get()
+
+
+
+
+
+
+
+
+httpServer.listen(3000, () => {
+    console.log("Server is running on port 3000")
+})
